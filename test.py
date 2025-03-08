ee05035b05365aa8fc5ceb2d4423fb20f6696e3d
Tanish_Lal <162808280+TanishLal@users.noreply.github.com>
2025-03-08 16:49:29 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.models import model_from_json
-with open(r"D:\newFolder\Shoplifting-Detection\CODE\shoplifting_model.json", "r") as json_file:
-    loaded_model_json = json_file.read()
-abuse_model = model_from_json(loaded_model_json)
-abuse_model.load_weights(weight_abuse)
